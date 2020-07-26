67126a698104c99bbf97cadd18092f51c763c186
miniBIGboss <minibigboss2@gmail.com>
2020-07-26 19:37:38 +0300

    Написана часть кода

---
+++ MultiplesOf.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Project_Euler
+{
+    public static class MultiplesOf
+    {
+        public static void Start()
+        {
+            Console.WriteLine("Эта программа находит сумму всех чисел кратных х и y, которые меньше z");
+            Console.WriteLine("Вам будет предложенно ввести три целых числа");
+            Action();
+        }
+
+        public static List<int> Action()
+        {
+            List<int> inputMultiplse = new List<int>();
+            string input;
+
+            do
+            {
+                Console.WriteLine("Введите число: ");
+                input = Console.ReadLine();
+                inputMultiplse.Add(Convert.ToInt32(input));
+            } while (inputMultiplse.Count < 3);
+
+            inputMultiplse.ForEach(Console.WriteLine);
+
+
+            return inputMultiplse;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Project_Euler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MultiplesOf.Start();
         }
     }
 }
